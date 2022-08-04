c7a5ebc35798e17afc5a09fd44de737a90e011c8
Hasan Ali <hasan@lacritz.ai>
2022-08-04 17:51:21 +0530

    Completed assignment 7

--- assignments/loops/loops3.c
+++ assignments/loops/loops3.c
@@ -52,11 +52,120 @@ void fibbonacciNumberExistence(int number)
     }
 }
 
+void findHCF(int a,int b)
+{
+    int gcd;
+    for(int i = 2 ; i <= a && i <= b ; i++)
+    {
+        if(a % i == 0 && b % i == 0)
+        {
+            gcd = i;
+        }
+    }
+    printf("GCD of %d and %d is %d ",a,b,gcd);
+
+
+}
+
+void checkCoPrime(int a,int b)
+{
+    // If the HCF is only 1 then they are called co prime
+    int isCoPrime = 1;
+
+    for(int i = 2 ; i < (a > b ? a : b) ; i++)
+    {
+        if(a % i == 0 && b % i == 0)
+        {
+            isCoPrime = 0;
+            break;
+        }
+    } 
+    if(isCoPrime)
+        printf("Co prime");
+    else
+        printf("Non Co prime");
+}
+
+int isPrime(int number)
+{
+    for(int i = 2 ; i < number ; i++)
+    {
+        if(number % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+void printPrimeUnder100()
+{
+    for(int i = 1 ; i <= 100 ; i++)
+    {
+        if(isPrime(i))
+            printf("%d ",i);
+    }
+}
+
+void printPrimeBetweenTwoNumbers(int a , int b)
+{
+    for(int i = a ; i <= b ;i++)
+    {
+        if(isPrime(i))
+            printf("%d ",i);
+    }
+}
+
+void printNextprimeNumber(int number)
+{
+    int i = number;
+    while(i)
+    {
+        if(isPrime(i))
+        {
+            if(i > number)
+            {
+                printf("%d ",i);
+                break;
+            }
+        }
+        i++;
+    }
+}
+
+int checkArmstrongNumber(int number)
+{
+    int cube = 0,originalNumber = number;
+    while(number > 0)
+    {
+        int rem = number % 10;
+        cube = cube + rem * rem * rem;
+        number/=10;
+    }
+    if(cube == originalNumber)
+        return 1;
+    return 0;
+
+}
+
+void printArmstrongNumbers()
+{
+    for(int i = 0 ; i <= 1000 ; i++)
+    {
+        if(checkArmstrongNumber(i))
+            printf("%d ",i);
+    }
+}
+
 int main()
 {
     int number = 0;
     // for(number = 0 ; number <= 10; number++)
         // findNthFibbonacciTerm(number);
         // findNthFibbonacciTerm(i);
-    fibbonacciNumberExistence(4);
+    // fibbonacciNumberExistence(4);
+    // findHCF(100,120);
+    // checkCoPrime(21,27);
+    // printPrimeUnder100();
+    // printPrimeBetweenTwoNumbers(1,10);
+    // printNextprimeNumber(3);
+    // printArmstrongNumbers();
 }
