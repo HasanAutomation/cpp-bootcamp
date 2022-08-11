6903b4b54395e34bca0595c9893354e1bca3dd67
Hasan Ali <hasan@lacritz.ai>
2022-08-11 21:25:10 +0530

    Formatted

--- assignments/functions/index.c
+++ assignments/functions/index.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
 double circleArea(int radius)
 {
     const float PI = 3.14;
     double area = PI * radius * radius;
-    printf("%lf",area);
+    printf("%lf", area);
     return area;
 }
 
-double simpleInterest(int principal,int rate,int time)
+double simpleInterest(int principal, int rate, int time)
 {
     const float interest = (principal * rate * time) / 100;
-    printf("%0.2lf",interest);
+    printf("%0.2lf", interest);
     return interest;
 }
 
@@ -20,13 +20,14 @@ int isEven(int number)
     return number % 2 == 0 ? 1 : 0;
 }
 
-int isDigitPresent(int number,int item)
+int isDigitPresent(int number, int item)
 {
-    while(number > 0)
+    while (number > 0)
     {
         int rem = number % 10;
-        if(rem == item) return 1;
-        number/=10;
+        if (rem == item)
+            return 1;
+        number /= 10;
     }
     return 0;
 }
@@ -35,11 +36,11 @@ void printPrimeFactors(int number)
     int index = 0;
     int i = 2;
 
-    while(number > 1)
+    while (number > 1)
     {
-        if(number % i == 0)
+        if (number % i == 0)
         {
-            printf("%d ",i);
+            printf("%d ", i);
             number = number / i;
         }
         else
@@ -51,11 +52,11 @@ int main()
 {
     circleArea(2);
     printf("\n");
-    simpleInterest(100000,10,5);
+    simpleInterest(100000, 10, 5);
     printf("\n");
-    printf("%d",isEven(9));
+    printf("%d", isEven(9));
     printf("\n");
-    printf("%d",isDigitPresent(123,0));
+    printf("%d", isDigitPresent(123, 0));
     printf("\n");
     printPrimeFactors(315);
 }
